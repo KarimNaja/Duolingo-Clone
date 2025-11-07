33ec19b15cdbb0fea442f6b995462712d1c819ce
KarimNaja <karimnaja3@gmail.com>
2025-11-07 18:08:02 +0300

    Learning Commit Break

---
+++ scripts/seed.ts
@@ -0,0 +1,20 @@
+import { neon } from "@neondatabase/serverless"
+import "dotenv/config"
+import { drizzle } from "drizzle-orm/neon-http"
+import * as schema from "../db/schema"
+
+const sql = neon(process.env.DATABASE_URL!)
+
+const db = drizzle(sql, { schema })
+
+const main = async () => {
+    try{
+        console.log("Seeding database")
+
+        await db.delete(schema.courses)
+        await db.delete(schema.userProgress)
+    } catch (error) {
+        console.error(error)
+        throw new Error("Failed to seed the database")
+    }
+}
